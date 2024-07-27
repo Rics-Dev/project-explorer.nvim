7f6661e58b4905920b7e73ea88712a301e970a84
racimfethallah <ricdev.io@gmail.com>
2024-07-27 23:30:18 +0100

    update for init

--- lua/project_explorer/init.lua
+++ lua/project_explorer/init.lua
@@ -4,10 +4,15 @@ function M.setup(opts)
 	opts = opts or {}
 	-- You can add any configuration options here
 
-	-- Create the user command
-	vim.api.nvim_create_user_command("ProjectExplorer", function(cmd_opts)
-		require("project_explorer.explorer").explore_projects(cmd_opts)
+	-- Ensure the command is created even if the plugin is lazy-loaded
+	vim.api.nvim_create_user_command("ProjectExplorer", function()
+		require("project_explorer.explorer").explore_projects()
 	end, {})
+end
+
+-- Add this function to allow direct calling
+function M.explore_projects()
+	require("project_explorer.explorer").explore_projects()
 end
 
 return M
