28d33ea1773703239d94e8326dd4bf148f6ac757
racimfethallah <ricdev.io@gmail.com>
2024-07-28 14:16:26 +0100

    improved recreate picker

--- lua/project_explorer/explorer.lua
+++ lua/project_explorer/explorer.lua
@@ -96,63 +96,65 @@ local function change_working_directory(prompt_bufnr)
 	--vim.cmd("Explore")
 end
 
-local function add_project()
+local function add_project(callback)
 	local project_name = vim.fn.input("Enter new project name: ")
 	if project_name == "" then
-		print("Project name cannot be empty.")
+		--		print("Project name cannot be empty.")
+		callback()
 		return
 	end
-
 	local base_dir = vim.fn.input("Enter base directory for the new project: ", "~/dev/")
 	local full_path = vim.fn.expand(base_dir .. "/" .. project_name)
-
 	print("Attempting to create directory: " .. full_path)
-
 	local success, error_msg = vim.fn.mkdir(full_path, "p")
 	if success == 1 then
 		print("Project directory created: " .. full_path)
-		-- Check if the directory exists before changing to it
 		if vim.fn.isdirectory(full_path) == 1 then
-			vim.cmd("cd " .. vim.fn.fnameescape(full_path))
-			vim.cmd("bdelete")
-			vim.cmd("Neotree" .. vim.fn.fnameescape(full_path))
-
-			print("Changed working directory to: " .. full_path)
+			print("Project created successfully: " .. full_path)
 		else
 			print("Directory created but not found. Current working directory: " .. vim.fn.getcwd())
 		end
 	else
 		print("Failed to create project directory. Error: " .. tostring(error_msg))
 	end
+	callback()
 end
 
 local function explore_projects(opts)
 	opts = opts or {}
-	pickers
-		.new(opts, {
-			prompt_title = "Project Explorer",
-			finder = create_finder(),
-			previewer = false,
-			sorter = telescope_config.generic_sorter(opts),
-			attach_mappings = function(prompt_bufnr, map)
-				local on_project_selected = function()
-					change_working_directory(prompt_bufnr)
-				end
-				actions.select_default:replace(on_project_selected)
 
-				map("i", "<C-a>", function()
-					--					actions.close(prompt_bufnr)
-					add_project()
-				end)
-				map("n", "<C-a>", function()
-					--					actions.close(prompt_bufnr)
-					add_project()
-				end)
+	local function recreate_picker()
+		pickers
+			.new(opts, {
+				prompt_title = "Project Explorer",
+				finder = create_finder(),
+				previewer = false,
+				sorter = telescope_config.generic_sorter(opts),
+				attach_mappings = function(prompt_bufnr, map)
+					local on_project_selected = function()
+						change_working_directory(prompt_bufnr)
+					end
+					actions.select_default:replace(on_project_selected)
 
-				return true
-			end,
-		})
-		:find()
+					map("i", "<C-a>", function()
+						--						actions.close(prompt_bufnr)
+						add_project(function()
+							recreate_picker()
+						end)
+					end)
+					map("n", "<C-a>", function()
+						--						actions.close(prompt_bufnr)
+						add_project(function()
+							recreate_picker()
+						end)
+					end)
+					return true
+				end,
+			})
+			:find()
+	end
+
+	recreate_picker()
 end
 
 -- Expose the main function
