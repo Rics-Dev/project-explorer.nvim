344fed45500e39ffdc4c14f4ffef2146ea98c7e9
racimfethallah <ricdev.io@gmail.com>
2024-07-28 01:00:50 +0100

    updated neotree impl

--- lua/project_explorer/explorer.lua
+++ lua/project_explorer/explorer.lua
@@ -91,7 +91,7 @@ local function change_working_directory(prompt_bufnr)
 	actions.close(prompt_bufnr)
 	vim.cmd("cd " .. dir)
 	vim.cmd("bdelete")
-	vim.cmd("Neotree")
+	vim.cmd("Neotree" .. dir)
 end
 
 local function explore_projects(opts)
