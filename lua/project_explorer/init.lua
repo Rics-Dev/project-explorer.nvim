a66ec2d1fb5cb585fec7cd14ba0033ea30ccab7b
racimfethallah <ricdev.io@gmail.com>
2024-07-30 13:17:43 +0100

    updated session management

--- lua/project_explorer/init.lua
+++ lua/project_explorer/init.lua
@@ -1,13 +1,27 @@
 local M = {}
-
 function M.setup(opts)
 	opts = opts or {}
 	local config = require("project_explorer.config")
 	config.config = vim.tbl_deep_extend("force", config.config, opts)
-
 	vim.api.nvim_create_user_command("ProjectExplorer", function(cmd_opts)
 		require("project_explorer.explorer").explore_projects(cmd_opts)
 	end, {})
-end
 
+	-- Create sessions directory if it doesn't exist
+	local session_dir = vim.fn.stdpath("data") .. "/sessions"
+	if vim.fn.isdirectory(session_dir) == 0 then
+		vim.fn.mkdir(session_dir, "p")
+	end
+
+	-- Set up autocmd to save session on exit
+	vim.api.nvim_create_autocmd("VimLeavePre", {
+		callback = function()
+			local session_file = vim.fn.stdpath("data")
+				.. "/sessions/"
+				.. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+				.. ".vim"
+			vim.cmd("mksession! " .. session_file)
+		end,
+	})
+end
 return M
